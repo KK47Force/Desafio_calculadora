2bb6151dd3180469a4b71e171c29005f14140707
KK47Force <klebersonksp10@gmail.com>
2025-07-27 22:32:00 -0300

    adicionado, a url inicial

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(
+        url='/accounts/login/', permanent=False), name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('tela_login.urls')),
     path('calculadora/', include('calculadora.urls')),
